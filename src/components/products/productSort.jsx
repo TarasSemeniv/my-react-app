73f4d08ccaf46cb88986ce123e4086ad91e5e27f
Тарас Семенів <tarassemeniv2@gmail.com>
2025-08-17 13:58:33 +0200

    added sorting by price, rating and name

---
+++ src/components/products/productSort.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ProductSort = ({ sort, setSort, sortDirection, setSortDirection }) => {
+    return (
+        <div>
+            <div className="product-filter">
+            <span>Sort by:</span>
+            <select value={sortDirection} onChange={(e) => setSortDirection(e.target.value)}>
+                <option value="Up">↑</option>
+                <option value="Down">↓</option>
+            </select>
+            <select value={sort} onChange={(e) => setSort(e.target.value)}>
+                <option value="Price">Price</option>
+                <option value="Rating">Rating</option>
+                <option value="Name">Name</option>
+            </select>
+        </div>
+        </div>
+    );
+}
+
+export default ProductSort;
