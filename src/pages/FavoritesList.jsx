9f06d9e5b0ab4795bbe22192b16996f1994bbe50
Тарас Семенів <tarassemeniv2@gmail.com>
2025-08-24 14:04:10 +0200

    added components header and favoritesProducts

---
+++ src/pages/FavoritesList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavoritesList = () => {
+    return (
+        <div>
+            <h1>Favorite Products</h1>
+        </div>
+    );
+}
+
+export default FavoritesList;
